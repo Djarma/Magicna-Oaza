c7d660de9dbf1a0c09d9bb7c3e9ccea7760b2ce5
djarma <kutri.denisz@gmail.com>
2019-02-25 18:55:11 +0100

    Header done

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     });
     
     
-    //ANIMATION 
+    // ANIMATION 
     
     function animation() {
         var windowHight = $(window).height();
@@ -31,6 +31,51 @@ $(document).ready(function () {
     $(window).scroll(function () {
         animation();
     });
+    
+    // SUBMENU
+
+    $('.navbar-nav').on('click', '.toggler-1', function (e) {
+        e.preventDefault();
+        $('.toggle-submenu').toggleClass('fa-angle-down fa-angle-up');
+        $('.toggle-submenu').parent().next('.submenu').slideToggle();
+        $('.search-form').slideUp();
+    });
+    
+    $('.level-1').on('click', '.toggler-2', function (e) {
+        e.preventDefault();
+        $('.toggle-submenu-2').toggleClass('fa-angle-double-right fa-angle-double-left');
+        $('.toggle-submenu-2').parent().next('.submenu-2').slideToggle();
+    });
+    
+    
+    
+    // SEARCH 
+    
+    $('.search-icon').click(function () {
+        $(this).next().slideToggle();
+        $('.small-cart').removeClass('active');
+        $('.body-overlay').hide(0);
+        $('.submenu').slideUp();
+        $('.toggle-submenu').removeClass('fa-angle-up').addClass('fa-angle-down');
+    });
+
+
+
+   // SMALL CART
+   
+   $('.cart').click(function () {
+        $('.small-cart').addClass('active');
+        $('.body-overlay').show(0);
+        $('.search-form').slideUp();
+        $('.submenu').slideUp();
+        $('.toggle-submenu').removeClass('fa-angle-up').addClass('fa-angle-down');
+    });
+    
+   $('.body-overlay').click(function () {
+        $('.small-cart').removeClass('active');
+        $('.body-overlay').hide();
+    });
+
 
 });//end document.ready
 
